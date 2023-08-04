cefb4984fd6c7707ca5a7349d01058aecf99ee74
Marcel Suter <marcel.suter@bzz.ch>
2023-08-04 08:08:54 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    # write your program logic here and remove the line "pass"
-    pass
+    bills = [200, 100, 75, 25, 1]
+    input_string = input("Gewünschter Betrag > ")
+    amount = int(input_string)
+    index = 0
+    while amount > 0:
+        if bills[index] > amount:
+            index += 1
+        else:
+            print(str(bills[index]) + ' KD')
+            amount = amount - bills[index]
 
 
 if __name__ == '__main__':
